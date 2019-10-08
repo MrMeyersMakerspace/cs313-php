b5198a032b906567d69e07ebdceba01866a9c7c8
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-10-07 20:02:38 -0600

    AJAX testing

--- web/prove03/siteData.php
+++ web/prove03/siteData.php
@@ -10,12 +10,12 @@
 		$_SESSION["prusa"] = $_GET["prusa"];
 
 		$printerArray = array(
-				"mini"		=>	$_GET["mini"],   //$_SESSION["mini"],
-				"ender"		=>	$_GET["ender"],   //$_SESSION["ender"],
-				"voxel"		=>	$_GET["voxel"],   //$_SESSION["voxel"],
-				"cr10"		=>	$_GET["cr10"],   //$_SESSION["cr10"],
-				"cr10s"		=>	$_GET["cr10s"],   //$_SESSION["cr10s"],
-				"prusa"		=>	$_GET["prusa"]   //$_SESSION["prusa"]
+				"mini"		=>	1,   //$_GET["mini"],   //$_SESSION["mini"],
+				"ender"		=>	1,   //$_GET["ender"],   //$_SESSION["ender"],
+				"voxel"		=>	1,   //$_GET["voxel"],   //$_SESSION["voxel"],
+				"cr10"		=>	1,   //$_GET["cr10"],   //$_SESSION["cr10"],
+				"cr10s"		=>	1,   //$_GET["cr10s"],   //$_SESSION["cr10s"],
+				"prusa"		=>	1   //$_GET["prusa"]   //$_SESSION["prusa"]
 		);
 
 		echo json_encode($printerArray);
