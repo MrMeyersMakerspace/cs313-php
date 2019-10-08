5041b225632798629f31fb5db8c0f7a79f5da4d5
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-10-07 20:47:13 -0600

    test

--- web/prove03/siteData.php
+++ web/prove03/siteData.php
@@ -1,13 +1,13 @@
 ﻿<?php
     //  Start the session
-    session_start();
+    /*session_start();
 		
 		$_SESSION["Smini"]  = $_GET["mini"];
 		$_SESSION["Sender"] = $_GET["ender"];
 		$_SESSION["Svoxel"] = $_GET["voxel"];
 		$_SESSION["Scr10"]  = $_GET["cr10"];
 		$_SESSION["Scr10s"] = $_GET["cr10s"];
-		$_SESSION["Sprusa"] = $_GET["prusa"];
+		$_SESSION["Sprusa"] = $_GET["prusa"];*/
 
 		$printerArray = array(
 				"Amini"		=>	$_GET["mini"],
