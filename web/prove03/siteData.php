4b0c9b2ea0d7aa1e49d7b7981d2791b4dba68346
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-10-07 19:55:40 -0600

    Testing PHP changes

--- web/prove03/siteData.php
+++ web/prove03/siteData.php
@@ -2,20 +2,20 @@
     //  Start the session
     session_start();
 		
-		$_SESSION["mini"] = $_REQUEST["mini"];
+		$_SESSION["mini"]  = $_REQUEST["mini"];
 		$_SESSION["ender"] = $_REQUEST["ender"];
 		$_SESSION["voxel"] = $_REQUEST["voxel"];
-		$_SESSION["cr10"] = $_REQUEST["cr10"];
+		$_SESSION["cr10"]  = $_REQUEST["cr10"];
 		$_SESSION["cr10s"] = $_REQUEST["cr10s"];
 		$_SESSION["prusa"] = $_REQUEST["prusa"];
 
 		$printerArray = array(
-				"mini"		=>	$_SESSION["mini"],
-				"ender"		=>	$_SESSION["ender"],
-				"voxel"		=>	$_SESSION["voxel"],
-				"cr10"		=>	$_SESSION["cr10"],
-				"cr10s"		=>	$_SESSION["cr10s"],
-				"prusa"		=>	$_SESSION["prusa"]
+				"mini"		=>	$_REQUEST["mini"];   //$_SESSION["mini"],
+				"ender"		=>	$_REQUEST["ender"];   //$_SESSION["ender"],
+				"voxel"		=>	$_REQUEST["voxel"];   //$_SESSION["voxel"],
+				"cr10"		=>	$_REQUEST["cr10"];   //$_SESSION["cr10"],
+				"cr10s"		=>	$_REQUEST["cr10s"];   //$_SESSION["cr10s"],
+				"prusa"		=>	$_REQUEST["prusa"];   //$_SESSION["prusa"]
 		);
 
 		echo json_encode($printerArray);
