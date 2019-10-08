db0b9da5bbdf196a0c72b21c769858e3af9e20ea
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-10-07 20:00:57 -0600

    Update siteData.php

--- web/prove03/siteData.php
+++ web/prove03/siteData.php
@@ -2,20 +2,20 @@
     //  Start the session
     session_start();
 		
-		$_SESSION["mini"]  = $_REQUEST["mini"];
-		$_SESSION["ender"] = $_REQUEST["ender"];
-		$_SESSION["voxel"] = $_REQUEST["voxel"];
-		$_SESSION["cr10"]  = $_REQUEST["cr10"];
-		$_SESSION["cr10s"] = $_REQUEST["cr10s"];
-		$_SESSION["prusa"] = $_REQUEST["prusa"];
+		$_SESSION["mini"]  = $_GET["mini"];
+		$_SESSION["ender"] = $_GET["ender"];
+		$_SESSION["voxel"] = $_GET["voxel"];
+		$_SESSION["cr10"]  = $_GET["cr10"];
+		$_SESSION["cr10s"] = $_GET["cr10s"];
+		$_SESSION["prusa"] = $_GET["prusa"];
 
 		$printerArray = array(
-				"mini"		=>	$_REQUEST["mini"],   //$_SESSION["mini"],
-				"ender"		=>	$_REQUEST["ender"],   //$_SESSION["ender"],
-				"voxel"		=>	$_REQUEST["voxel"],   //$_SESSION["voxel"],
-				"cr10"		=>	$_REQUEST["cr10"],   //$_SESSION["cr10"],
-				"cr10s"		=>	$_REQUEST["cr10s"],   //$_SESSION["cr10s"],
-				"prusa"		=>	$_REQUEST["prusa"]   //$_SESSION["prusa"]
+				"mini"		=>	$_GET["mini"],   //$_SESSION["mini"],
+				"ender"		=>	$_GET["ender"],   //$_SESSION["ender"],
+				"voxel"		=>	$_GET["voxel"],   //$_SESSION["voxel"],
+				"cr10"		=>	$_GET["cr10"],   //$_SESSION["cr10"],
+				"cr10s"		=>	$_GET["cr10s"],   //$_SESSION["cr10s"],
+				"prusa"		=>	$_GET["prusa"]   //$_SESSION["prusa"]
 		);
 
 		echo json_encode($printerArray);
