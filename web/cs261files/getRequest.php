a39580e45e8e1ba4b2ef76bf951d2e8f18853c8a
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-10-12 12:07:22 -0600

    PHP documents for CS 261

---
+++ web/cs261files/getRequest.php
@@ -0,0 +1,3 @@
+<?php
+		echo "Your name is " . $_GET["nameG"] . ", your favorite color is " . $_GET["favColorG"] . ", and your favorite Ice Cream is " . $_GET["iceCreamG"] . ".";
+?>
---
+++ web/cs261files/postRequest.php
@@ -0,0 +1,3 @@
+<?php
+		echo "Your name is " . $_POST["nameP"] . ", your favorite color is " . $_POST["favColorP"] . ", and your favorite Ice Cream is " . $_POST["iceCreamP"] . ".";
+?>
