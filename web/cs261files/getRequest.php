3ffbe24be52892c44fa49f8e5a5eee4e120f3506
Meyers-John <44011551+MrMeyersMakerspace@users.noreply.github.com>
2019-10-12 14:07:58 -0600

    fixed spelling

--- web/cs261files/getRequest.php
+++ web/cs261files/getRequest.php
@@ -1,3 +1,3 @@
 <?php
-		echo "Your name is " . $_GET["nameG"] . ", your favorite color is " . $_GET["favColorG"] . ", and your favorite Ice Cream is " . $_GET["iceCreamG"] . ".";
+		echo "Your name is " . $_GET["nameG"] . ", your favorite color is " . $_GET["favColorG"] . ", and your favorite ice cream is " . $_GET["iceCreamG"] . ".";
 ?>
--- web/cs261files/postRequest.php
+++ web/cs261files/postRequest.php
@@ -1,3 +1,3 @@
 <?php
-		echo "Your name is " . $_POST["nameP"] . ", your favorite color is " . $_POST["favColorP"] . ", and your favorite Ice Cream is " . $_POST["iceCreamP"] . ".";
+		echo "Your name is " . $_POST["nameP"] . ", your favorite color is " . $_POST["favColorP"] . ", and your favorite ice cream is " . $_POST["iceCreamP"] . ".";
 ?>
