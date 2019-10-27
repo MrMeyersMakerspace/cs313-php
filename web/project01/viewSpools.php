b96af98c39f59c7952419887ab52a2493491a2a2
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-10-26 23:35:51 -0600

    Update viewSpools.php

--- web/project01/viewSpools.php
+++ web/project01/viewSpools.php
@@ -39,7 +39,7 @@ $db = get_db();
 
             <h1>View Current Spools</h1>
 
-            <h3 style="text-align:center;">Search by plastic type, color of filament, or display all available spools!</h3>
+            <h2 style="text-align:center;">Search by plastic type, color of filament, or display all available spools!</h2>
             <form>
                 Type of Plastic:
                 <select name="plasticType">
