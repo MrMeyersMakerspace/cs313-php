2c07eceb5cf775862324a9dddc34e770ab55cf1a
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-10-30 17:29:30 -0600

    test

--- web/team07/submit.php
+++ web/team07/submit.php
@@ -6,9 +6,10 @@ $db = get_db();
 $username = $_POST['username'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-$query = 'INSERT INTO teamuser (username, password) VALUES :username, :password';
+$query = 'INSERT INTO teamuser (username, password) VALUES (:username, :password)';
 $statement = $db->prepare($query);
 $statement->bindValue(':username', $username);
 $statement->bindValue(':password', $password);
 $statement->execute();
+
 ?>
