775dfd1ebdfa971c4d7dc06ee16b64e491544824
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-11-02 23:36:55 -0600

    Update sign-up.php

--- web/project01/sign-up.php
+++ web/project01/sign-up.php
@@ -12,17 +12,27 @@ session_start();
     <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-    <p>Please the following information for your new account.  Once submitted you will have to wait for approval by Maker Meyers before being able to login.</p>
-    <form method="POST" action="submit.php">
-        <label>Display Name:</label><br />
-        <input type="text" name="display_name" id="display_name" /><br />
-        <label>Email:</label><br />
-        <input type="text" name="email" id="email" /><br />
-        <label>Username:</label><br />
-        <input type="text" name="username" id="username"/><br />
-        <label>Password:</label><br />
-        <input type="text" name="password" id="password"/><br />
-        <button type="submit">Submit New Account</button>
-    </form>
+    <p>Please fill out the following information for your new account.  Once submitted you will have to wait for approval by Maker Meyers before being able to login.</p>
+    <div class="form">
+        <form method="POST" action="submit.php">
+            <label>Display Name:</label>
+            <br />
+            <input type="text" name="display_name" id="display_name" />
+            <br />
+            <label>Email:</label>
+            <br />
+            <input type="text" name="email" id="email" />
+            <br />
+            <label>Username:</label>
+            <br />
+            <input type="text" name="username" id="username" />
+            <br />
+            <label>Password:</label>
+            <br />
+            <input type="text" name="password" id="password" />
+            <br />
+            <button type="submit">Submit New Account</button>
+        </form>
+    </div>
 </body>
 </html>
