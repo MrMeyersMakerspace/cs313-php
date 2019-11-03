90e3b460f9117c792bc152ee8ae4a1ae34b998c4
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-11-03 02:25:13 -0700

    test new method for solving username error

--- web/project01/submit.php
+++ web/project01/submit.php
@@ -11,15 +11,25 @@ $email = htmlspecialchars($_POST['email']);
 $username = htmlspecialchars($_POST['username']);
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-$query = 'INSERT INTO tempusers (display_name, email, username, password) VALUES (:display_name, :email, :username, :password)';
-$statement = $db->prepare($query);
-$statement->bindValue(':display_name', $display_name);
-$statement->bindValue(':email', $email);
-$statement->bindValue(':username', $username);
-$statement->bindValue(':password', $password);
-$statement->execute();
+$result = mysql_query("SELECT * FROM tempusers WHERE username=$username");
+$num_rows = mysql_num_rows($result);
 
-$_SESSION['error'] = "The account for $display_name has been submitted for approval by Maker Meyers.<br/>Try signing in later or contact Maker Meyers to speed up approval process.";
-header("Location: index.php");
-die();
+if ($num_rows) {
+    $_SESSION['error'] = "The username $username has already been used.<br/>Please select a different username and try again.";
+    header("Location: index.php");
+    die();
+} else {
+
+    $query = 'INSERT INTO tempusers (display_name, email, username, password) VALUES (:display_name, :email, :username, :password)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':display_name', $display_name);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':username', $username);
+    $statement->bindValue(':password', $password);
+    $statement->execute();
+
+    $_SESSION['error'] = "The account for $display_name has been submitted for approval by Maker Meyers.<br/>Try signing in later or contact Maker Meyers to speed up approval process.";
+    header("Location: index.php");
+    die();
+}
 ?>
