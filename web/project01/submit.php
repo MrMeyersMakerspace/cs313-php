07edc8fa03ebe9393461259b830fa757c4d13783
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-11-03 02:21:34 -0700

    revert back to old setup

--- web/project01/submit.php
+++ web/project01/submit.php
@@ -6,35 +6,20 @@ session_start();
 require("dbConnect.php");
 $db = get_db();
 
-$counter = 0;
 $display_name = htmlspecialchars($_POST['display_name']);
 $email = htmlspecialchars($_POST['email']);
 $username = htmlspecialchars($_POST['username']);
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-// Gets the current username
-$sql = "SELECT username FROM tempusers WHERE username = $username";
-foreach ($db->query($sql) as $row1) {
-    $currentUsername = $row1['username'];
-    $counter = $counter + 1;
-}
+$query = 'INSERT INTO tempusers (display_name, email, username, password) VALUES (:display_name, :email, :username, :password)';
+$statement = $db->prepare($query);
+$statement->bindValue(':display_name', $display_name);
+$statement->bindValue(':email', $email);
+$statement->bindValue(':username', $username);
+$statement->bindValue(':password', $password);
+$statement->execute();
 
-if ($counter == 0) {
-    $_SESSION['error'] = "The username $username has has already been taken.<br/>Please try again.";
-    header("Location: index.php");
-    die();
-} else {
-    $query = 'INSERT INTO tempusers (display_name, email, username, password) VALUES (:display_name, :email, :username, :password)';
-    $statement = $db->prepare($query);
-    $statement->bindValue(':display_name', $display_name);
-    $statement->bindValue(':email', $email);
-    $statement->bindValue(':username', $username);
-    $statement->bindValue(':password', $password);
-    $statement->execute();
-
-
-    $_SESSION['error'] = "The account for $display_name has been submitted for approval by Maker Meyers.<br/>Try signing in later or contact Maker Meyers to speed up approval process.";
-    header("Location: index.php");
-    die();
-}
+$_SESSION['error'] = "The account for $display_name has been submitted for approval by Maker Meyers.<br/>Try signing in later or contact Maker Meyers to speed up approval process.";
+header("Location: index.php");
+die();
 ?>
