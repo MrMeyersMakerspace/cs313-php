e4f35104e233b5fcba45833e5732dfb8bbe2ae18
Meyers-John <mrmeyersmakerspace@gmail.com>
2019-11-03 02:00:19 -0700

    Update submit.php

--- web/project01/submit.php
+++ web/project01/submit.php
@@ -11,13 +11,26 @@ $email = htmlspecialchars($_POST['email']);
 $username = htmlspecialchars($_POST['username']);
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-$query = 'INSERT INTO tempusers (display_name, email, username, password) VALUES (:display_name, :email, :username, :password)';
+$query = 'SELECT username FROM tempusers WHERE username = :username';
 $statement = $db->prepare($query);
-$statement->bindValue(':display_name', $display_name);
-$statement->bindValue(':email', $email);
 $statement->bindValue(':username', $username);
-$statement->bindValue(':password', $password);
-$statement->execute();
+$result = $statement->execute();
+
+if ($result) {
+    $row = $statement->fetch();
+    $_SESSION['error'] = "The username $username has already been taken.<br/>Try again with a new username.";
+    header("Location: index.php");
+    die();
+
+} else {
+    $query = 'INSERT INTO tempusers (display_name, email, username, password) VALUES (:display_name, :email, :username, :password)';
+    $statement = $db->prepare($query);
+    $statement->bindValue(':display_name', $display_name);
+    $statement->bindValue(':email', $email);
+    $statement->bindValue(':username', $username);
+    $statement->bindValue(':password', $password);
+    $statement->execute();
+}
 
 $_SESSION['error'] = "The account for $display_name has been submitted for approval by Maker Meyers.<br/>Try signing in later or contact Maker Meyers to speed up approval process.";
 header("Location: index.php");
